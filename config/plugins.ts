bd4961f5fac8a9c1efae6e91e1ccd7a39bab8f62
Julio Varas Contreras <jvarascontreras@gmail.com>
2024-12-16 19:09:21 -0300

    Se usara Cloudinary de forma temporal para no perder las imagenes y continuar con las pruebas

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,16 +2,30 @@ export default ({ env }) => ({
     "video-field": {
         enabled: true,
     },
+    //     upload: {
+    //         config: {
+    //             provider: "local",
+    //             providerOptions: {
+    //                 sizeLimit: 1000000, // Límite de tamaño en bytes (1MB en este ejemplo)
+    //             },
+    //             actionOptions: {
+    //                 upload: {},
+    //                 delete: {},
+    //             },
+    //         },
+    //     },
     upload: {
         config: {
-            provider: "local",
+            provider: 'cloudinary',
             providerOptions: {
-                sizeLimit: 1000000, // Límite de tamaño en bytes (1MB en este ejemplo)
+                cloud_name: env('CLOUDINARY_NAME'),
+                api_key: env('CLOUDINARY_KEY'),
+                api_secret: env('CLOUDINARY_SECRET'),
             },
-            actionOptions: {
-                upload: {},
-                delete: {},
-            },
+        },
+        actionOptions: {
+            upload: {},
+            delete: {},
         },
     },
 });
