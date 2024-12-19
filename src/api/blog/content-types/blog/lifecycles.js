63b6457c8e1c0410ea0b7f692d102110421efb13
Julio Varas Contreras <jvarascontreras@gmail.com>
2024-12-18 21:21:20 -0300

    se vuelve a modificar archivo que crea los slug

--- src/api/blog/content-types/blog/lifecycles.js
+++ src/api/blog/content-types/blog/lifecycles.js
@@ -1,19 +1,16 @@
-// src/api/blog/content-types/blog/lifecycles.js
 const slugify = require('slugify');
 
 module.exports = {
     async beforeCreate(event) {
         const { data } = event.params;
-        if (data.blogTitle) {
-            // Genera un slug único basado en el título
-            data.slug = slugify(data.blogTitle, { lower: true, strict: true });
+        if (data.title) {
+            data.slug = slugify(data.title, { lower: true, strict: true });
         }
     },
     async beforeUpdate(event) {
         const { data } = event.params;
-        if (data.blogTitle) {
-            // Genera un slug único basado en el título
-            data.slug = slugify(data.blogTitle, { lower: true, strict: true });
+        if (data.title) {
+            data.slug = slugify(data.title, { lower: true, strict: true });
         }
     },
 };
