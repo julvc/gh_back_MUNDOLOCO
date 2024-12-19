d4fbe38a10d460d461914dbd03b12c9dbda3d68f
Julio Varas Contreras <jvarascontreras@gmail.com>
2024-12-19 11:01:41 -0300

    feat(api/blog/services/blog.ts): Add slug generation on blog creation

--- src/api/blog/services/blog.ts
+++ src/api/blog/services/blog.ts
@@ -3,5 +3,19 @@
  */
 
 import { factories } from '@strapi/strapi';
+import slugify from 'slugify';
 
-export default factories.createCoreService('api::blog.blog');
+export default factories.createCoreService('api::blog.blog', ({ strapi }) => ({
+    async beforeCreate(event) {
+        const { data } = event;
+
+        if (!data.slug) {
+            data.slug = slugify(data.title, {
+                lower: true,
+                replacement: '-',
+                remove: /[^a-z0-9]+/g,
+                trim: true
+            });
+        }
+    },
+}));
